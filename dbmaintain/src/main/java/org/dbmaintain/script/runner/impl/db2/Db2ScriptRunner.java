3ac245e30bbfaee37f4ae7a6beaa470d3d4a7b2a
hjaiswal <hemant.jaiswal@ideas.com>
2015-08-12 11:20:13 -0500

    aa

--- dbmaintain/src/main/java/org/dbmaintain/script/runner/impl/db2/Db2ScriptRunner.java
+++ dbmaintain/src/main/java/org/dbmaintain/script/runner/impl/db2/Db2ScriptRunner.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 package org.dbmaintain.script.runner.impl.db2;
 
 import org.dbmaintain.database.Database;
--- dbmaintain/src/test/java/org/dbmaintain/script/windows-script.sql
+++ dbmaintain/src/test/java/org/dbmaintain/script/windows-script.sql
@@ -1,4 +1,3 @@
-
 create table table1 (col1 smallint);
 create table table2 (col1 smallint);
 
